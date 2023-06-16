6912b270d408b38d7ec6ef47183beeeb83d0b81c
Artem-kn9z <68550950+Artem-kn9z@users.noreply.github.com>
2023-06-16 11:31:37 +0300

    Fixed bugs.

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -8,33 +8,34 @@ import java.nio.file.Path;
 
 public class FileReader {
 
-    String text = "";
+    private String text = "";
 
     public Profile getDataFromFile(File file) {
 
         Path filePath = Path.of(file.getPath());
         if (Files.exists(filePath)) {
+
             try {
                 byte[] bytes = Files.readAllBytes(filePath);
-                String text = new String(bytes);
+                text = new String(bytes);
 
-                text = text.replaceAll("\r\n", ": ");
-                System.out.println(text);
-
-                String[] words = text.split(": ");
-                Profile pf = new Profile();
-
-                for (int i = 1; i < words.length; i++) {
-                    if (words[i - 1].equals("Name")) pf.setName(words[i]);
-                    if (words[i - 1].equals("Age")) pf.setAge(Integer.parseInt(words[i]));
-                    if (words[i - 1].equals("Email")) pf.setEmail(words[i]);
-                    if (words[i - 1].equals("Phone")) pf.setPhone((long) Integer.parseInt(words[i]));
-                }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                System.err.print(e);
             }
-        }
 
-        return new Profile();
+            text = text.replace("\r\n", ": ");
+
+            String[] words = text.split(": ");
+            Profile pf = new Profile();
+
+            for (int i = 1; i < words.length; i++) {
+                if (words[i - 1].equals("Name")) pf.setName(words[i]);
+                if (words[i - 1].equals("Age")) pf.setAge(Integer.parseInt(words[i]));
+                if (words[i - 1].equals("Email")) pf.setEmail(words[i]);
+                if (words[i - 1].equals("Phone")) pf.setPhone((long) Integer.parseInt(words[i]));
+            }
+            return pf;
+        } else return new Profile();
+
     }
 }
